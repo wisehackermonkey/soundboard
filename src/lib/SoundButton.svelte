c3fd7054412c6736f0899e1c6c45b80f05269994
Oran C <oranbusiness@gmail.com>
2021-12-31 12:40:11 -0700

    first commit

---
+++ src/lib/SoundButton.svelte
@@ -0,0 +1,97 @@
+<script>
+	export let path = '';
+	// ;// = {"name":"","url":"https://www.myinstants.com/media/sounds/owenwowson5.mp3"};
+	export let title = '';
+	import TrackHeading from '$lib/TrackHeading.svelte';
+	import Controls from '$lib/Controls.svelte';
+	import { onMount } from 'svelte';
+	import {fade} from "svelte/transition";
+	$: loading = false;
+	// Get Audio track
+	let trackIndex = 0;
+	// $: console.log(trackIndex)
+	let audioFile; // =new Audio(path);
+	// audioFile.preload = "auto"
+	let trackTitle = title;
+
+	const loadTrack = () => {
+		audioFile = new Audio(path);
+		audioFile.onloadedmetadata = () => {
+			updateTime();
+		};
+
+		trackTitle = title;
+	};
+
+	let trackTimer;
+
+	function updateTime() {
+		if (audioFile.ended) {
+			toggleTimeRunning();
+		}
+	}
+
+	const toggleTimeRunning = () => {
+		if (audioFile.ended) {
+			isPlaying = false;
+			clearInterval(trackTimer);
+			console.log(`Ended = ${audioFile.ended}`);
+		} else {
+			trackTimer = setInterval(updateTime, 100);
+		}
+	};
+
+	// Controls
+	let isPlaying = false;
+	$: console.log(`isPlaying = ${isPlaying}`);
+
+	const playPauseAudio = () => {
+		if (audioFile.paused) {
+			toggleTimeRunning();
+			audioFile.play();
+			isPlaying = true;
+		} else {
+			toggleTimeRunning();
+			audioFile.pause();
+			isPlaying = false;
+		}
+	};
+
+	// Playlist
+	const handleTrack = (e) => {
+		if (!isPlaying) {
+			trackIndex = Number(e.target.dataset.trackId);
+			loadTrack();
+			playPauseAudio(); // auto play
+		} else {
+			isPlaying = false;
+			audioFile.pause();
+			loadTrack();
+			playPauseAudio(); // auto play
+		}
+	};
+	onMount(() => {
+		loadTrack();
+	});
+</script>
+
+<main>
+	<section transition:fade id="player-cont">
+		<TrackHeading {trackTitle} />
+
+		<Controls {isPlaying} on:playPause={playPauseAudio} />
+	</section>
+</main>
+
+<style>
+	#player-cont {
+		margin: 10px;
+		width: 96px;
+		/* height: 170px; */
+		padding: 0.7rem 1.5rem 10px;
+		box-shadow: 0 0 5px black;
+		background: #222;
+		color: #bbb;
+		border-radius: 5px 5px 0 0;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,81 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	import Counter from '$lib/Counter.svelte';
+	import { soundfx } from './soundfx.js';
+	import SoundButton from '$lib/SoundButton.svelte';
+	let name = 'world';
+
+	let formateFxURL = (fx_name) => {
+		return `https://www.myinstants.com/instant/${fx_name}/embed/`;
+	};
+	let formateAudioUrl = (fx_name) => `/sound/${fx_name}.mp3`;
+	let formateSoundObject = (obj) => {
+		obj.url = formateAudioUrl(obj.url);
+		return obj;
+	};
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+<main id="masonry-root">
+	<h1>Sound FX Board by oran</h1>
+
+	{#each soundfx as sound}
+		<!-- 		 <iframe width="110" height="200" src={formateFxURL(sound.url)} frameborder="0" scrolling="no"></iframe> -->
+		<!-- {console.log( formateAudioUrl(sound.url))}  -->
+		<SoundButton id="masonry-cell" title={sound.name} path={formateAudioUrl(sound.url)} />
+	{/each}
+</main>
+<section>
+	<h1>Sound FX Board by oran</h1>
+
+	<h1>
+		<div class="welcome">
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
+		</div>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
