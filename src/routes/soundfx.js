f08c567e868bb9e3dc7751f0179f78bd521bb3f9
Oran C <oranbusiness@gmail.com>
2021-12-31 16:12:26 -0700

    [add] new batch of sound fxs

--- src/routes/soundfx.js
+++ src/routes/soundfx.js
@@ -4,19 +4,22 @@ export const soundfx = [
     { "name": "owen wowson 2", "url": "owenwowson2", "color": "darkblue" },
     { "name": "owen wowson 4", "url": "owenwowson4", "color": "darkblue" },
     { "name": "owen wowson 3", "url": "owenwowson3", "color": "darkblue" },
+    { "name": "crickets", "url": "crickets", "color": "green" },
+
     { "name": "anime wow", "url": "anime-wow-sound-effect_Q6Q4ech", "color": "yellow" },
-    { "name": "oh yeah", "url": "oh-yeah", "color": "yellow" },
+    { "name": "wilhelm scream", "url": "wilhelmscream", "color": "#083d77" },
     { "name": "i like turtles", "url": "i-like-turtles", "color": "#083d77" },
     { "name": "gayyyy", "url": "gayyy", "color": "#083d77" },
-    { "name": "ill be back", "url": "ill-be-back","color": "#083d77" },
+    { "name": "ill be back", "url": "ill-be-back", "color": "#083d77" },
     { "name": "surprise motherfucka", "url": "surprise-motherfucka", "color": "#083d77" },
     { "name": "aint nobody got time", "url": "aint-nobody-got-time", "color": "#083d77" },
     { "name": "doh (homer)", "url": "doh", "color": "#083d77" },
     { "name": "finish him", "url": "finishhim", "color": "#083d77" },
     { "name": "spongebob a few moments later ", "url": "a-few-moments-later-hd", "color": "#083d77" },
     { "name": "bruh meme", "url": "movie_1_C2K5NH0", "color": "#083d77" },
-    { "name": "wilhelm scream", "url": "wilhelmscream", "color": "#083d77" },
+    { "name": "oh yeah", "url": "oh-yeah", "color": "#083d77" },
     { "name": "minecraft hurt", "url": "minecraft-hurt", "color": "#083d77" },
+    { "name": "combobreaker", "url": "combobreaker", "color": "#083d77" },
 
 
 
@@ -28,16 +31,20 @@ export const soundfx = [
     { "name": "bateria tum dum tss", "url": "bateria-tum-dum-tss", "color": "#DA4167" },
     { "name": "chewbacca", "url": "chewbacca", "color": "#DA4167" },
     { "name": "whip", "url": "whip", "color": "#DA4167" },
+    { "name": "crack the whip", "url": "crack_the_whip", "color": "#DA4167" },
     { "name": "buzzer", "url": "wrong-answer-sound-effect", "color": "#DA4167" },
     { "name": "wrong", "url": "wrong", "color": "#DA4167" },
+    { "name": "Sucess: family feud good answer", "url": "family-feud-good-answer", "color": "#DA4167" },
     { "name": "hitmarkermp3", "url": "hitmarker_2", "color": "#DA4167" },
     { "name": "bongo feet", "url": "bongo-feet", "color": "#DA4167" },
     { "name": "sonic ring", "url": "26f8b9_sonic_ring_sound_effect", "color": "#DA4167" },
     { "name": "sad trombone", "url": "sad-trombone", "color": "#DA4167" },
     { "name": "dun dun", "url": "dun-dun", "color": "#DA4167" },
     { "name": "ka ching", "url": "ka-ching", "color": "#DA4167" },
+    { "name": "batman transition", "url": "batman-transition-download-sound-link", "color": "#DA4167" },
+    { "name": "dramatic chipmunk", "url": "dramatic.swf", "color": "#DA4167" },
 
-    
+
     { "name": "error sound", "url": "error_CDOxCYm", "color": "red" },
     { "name": "ding-sound-effect", "url": "ding-sound-effect_2", "color": "red" },
 
@@ -48,12 +55,13 @@ export const soundfx = [
     { "name": "epic meal time crow sound", "url": "epic-mealtime-crow-sound", "color": "red" },
 
 
-    
+
 
     { "name": "elevator waiting", "url": "elevator_2jN6tnc", "color": "blue" },
+    { "name": "ferris bueller chicka chicka", "url": "ferris_bueller_chicka_chicka", "color": "" },
+    { "name": "rick rolled", "url": "epic", "color": "blue" },
     { "name": "eye of the tiger", "url": "eye-of-the-tiger", "color": "blue" },
     { "name": "shooting stars", "url": "untitled-1_qPskhfn", "color": "blue" },
-    { "name": "rick rolled", "url": "epic", "color": "blue" },
     { "name": "u cant touch this", "url": "mc-hammer-u-cant-touch-this", "color": "blue" },
     { "name": "jeopardy", "url": "jeopardy", "color": "#EBEBD3" },
     { "name": "snoop dogg meme", "url": "tmpbxydyrz3", "color": "#EBEBD3" },
@@ -62,6 +70,12 @@ export const soundfx = [
     { "name": "Mario Star Power", "url": "mario-star-power", "color": "#EBEBD3" },
     { "name": "epic sax button", "url": "epic-sax-guy-plays-for-57-minutes", "color": "#EBEBD3" },
     { "name": "sneaky snitch", "url": "sneaky-snitch", "color": "#EBEBD3" },
+    { "name": "all stars smash mough", "url": "all-stars-smash-mough", "color": "#EBEBD3" },
+    { "name": "batman", "url": "batman", "color": "#EBEBD3" },
+    { "name": "batman on drugs", "url": "batman_on_drugs-1", "color": "#EBEBD3" },
+    { "name": "birdtheword", "url": "birdtheword.swf", "color": "#EBEBD3" },
+    { "name": "directed by robert b", "url": "directed-by-robert-b_voI2Z4T", "color": "#EBEBD3" },
+
 
 
     { "name": "please stand by", "url": "please-stand-by-video-effect", "color": "#F4D35E" },
@@ -69,5 +83,24 @@ export const soundfx = [
     { "name": "sitcom audience laughing", "url": "sitcom-laughing", "color": "#F4D35E" },
     { "name": "door knocking sound effect", "url": "crazy-realistic-knocking-sound-troll-twitch-streamers_small", "color": "#F4D35E" },
     { "name": "kids cheering", "url": "kids_cheering", "color": "#F4D35E" },
+
+
+
+
+    { "name": "gta san andreas mission complete", "url": "gta-san-andreas-mission-complete-sound-hq", "color": "" },
+    { "name": "kamehameha dragon ball", "url": "kamehameha_dragon_ball", "color": "" },
+    { "name": "legendary za warudo road roller combo mp3cut", "url": "legendary-za-warudo-road-roller-combo-mp3cut", "color": "" },
+    { "name": "no god please no noooooooooo", "url": "no-god-please-no-noooooooooo", "color": "" },
+    { "name": "over 9000!", "url": "over9000", "color": "" },
+    { "name": "r2d2 doesnt feel that good", "url": "r2d2-doesnt-feel-that-good", "color": "" },
+    { "name": "seinfeld theme", "url": "seinfeld-theme_1", "color": "" },
+    { "name": "starwars", "url": "starwars", "color": "" },
+    { "name": "success", "url": "success", "color": "" },
+    { "name": "test whip crack", "url": "test-whip-crack", "color": "" },
+    { "name": "haha the simpsons nelsons haha", "url": "the-simpsons-nelsons-haha", "color": "" },
+    { "name": "whip sound effect ", "url": "whip-sound-effect-1", "color": "" },
+    { "name": "dialup", "url": "1990s-internet", "color": "" },
+    { "name": "the sexy song careless whispers", "url": "sexy_song_careless_whispers", "color": "" },
+    { "name": "taco bell bong", "url": "taco-bell-bong", "color": "" },
 
 ];
