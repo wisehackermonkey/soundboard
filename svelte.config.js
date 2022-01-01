e4d05392424878a4c4722a81b3c3165ac4ce0442
Oran C <oranbusiness@gmail.com>
2021-12-31 17:38:55 -0700

    [add]

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,23 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+ import adapter from '@sveltejs/adapter-static';
+ const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter:adapter({
-            // default options are shown
-            pages: 'build',
-            assets: 'build',
-            fallback: null
-        }), 
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
 		paths: { 
-			base: "/soundboard"
+			base: dev  ? '' : "/soundboard"
 		},
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		appDir: 'internal'
 	}
 };
 
